cfc409c5bbb647fbcb5613810e4c6d5a9c5cda45
tangchaofu <1027402361@qq.com>
2020-12-08 10:11:34 +0800

    '引入公共样式'

--- src/api/user.js
+++ src/api/user.js
@@ -1,13 +1,7 @@
 // import request from '@/utils/request'
 
-export function login(data) {
+export function login(data) {}
 
-}
+export function getInfo(token) {}
 
-export function getInfo(token) {
-
-}
-
-export function logout() {
-
-}
+export function logout() {}
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,21 +1,12 @@
 import axios from 'axios'
-// import { MessageBox, Message } from 'element-ui'
-// import store from '@/store'
-// import { getToken } from '@/utils/auth'
 
 // create an axios instance
-const service = axios.create({
-
-})
+const service = axios.create({})
 
 // request interceptor
-service.interceptors.request.use(
-
-)
+service.interceptors.request.use()
 
 // response interceptor
-service.interceptors.response.use(
-
-)
+service.interceptors.response.use()
 
 export default service
