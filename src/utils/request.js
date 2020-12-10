d8565f902e4a3cab1dad84169065eb14e327b29d
tangchaofu <1027402361@qq.com>
2020-12-10 11:18:40 +0800

    '配置生产环境'

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 // create an axios instance
-const service = axios.create({})
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
 
 // request interceptor
 service.interceptors.request.use()
