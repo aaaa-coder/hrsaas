65535437e409b63e4f5ab7ed73e2392ca1e122e7
tangchaofu <1027402361@qq.com>
2020-12-10 20:40:43 +0800

    '进度条完成'

--- src/permission.js
+++ src/permission.js
@@ -1,10 +1,14 @@
 import router from '@/router'
 import store from '@/store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 // 设置白名单
 const whiteList = ['/login', '/404']
 
 router.beforeEach((to, from, next) => {
+  // 设置进度条
+  NProgress.start()
   if (store.getters.token) {
     if (to.path === '/login') {
       next('/')
@@ -18,4 +22,11 @@ router.beforeEach((to, from, next) => {
       next('/login')
     }
   }
+  // 强制关闭
+  NProgress.done()
+})
+
+router.afterEach(() => {
+  // 修复跳转无法关闭的bug
+  NProgress.done()
 })
