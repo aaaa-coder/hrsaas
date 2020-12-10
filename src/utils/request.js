95d7781d610b10571a7bc3d008170f1e8db1521b
tangchaofu <1027402361@qq.com>
2020-12-10 19:50:14 +0800

    '环境处理完成'

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 // create an axios instance
 const service = axios.create({
+  // 设置开发基准路径
   baseURL: process.env.VUE_APP_BASE_API,
+  // 延时
   timeout: 5000
 })
 
@@ -10,6 +13,16 @@ const service = axios.create({
 service.interceptors.request.use()
 
 // response interceptor
-service.interceptors.response.use()
+service.interceptors.response.use(res => {
+  // 将获取的数据进行结构
+  const { success, message, data } = res.data
+  // 成功就返回数据
+  if (success) {
+    return data
+  } else {
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  }
+})
 
 export default service
