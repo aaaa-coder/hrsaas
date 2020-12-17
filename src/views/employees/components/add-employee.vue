657eb054811dd7e64f18c731b5cd8df4d797645e
tangchaofu <1027402361@qq.com>
2020-12-17 08:43:40 +0800

    '修改部门样式'

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -3,26 +3,62 @@
     <!-- 表单 -->
     <el-form label-width="120px" :model="formData" :rules="rules">
       <el-form-item label="姓名" prop="username">
-        <el-input v-model="formData.username" style="width:50%" placeholder="请输入姓名" />
+        <el-input
+          v-model="formData.username"
+          style="width: 50%"
+          placeholder="请输入姓名"
+        />
       </el-form-item>
       <el-form-item label="手机" prop="mobile">
-        <el-input v-model="formData.mobile" style="width:50%" placeholder="请输入手机号" />
+        <el-input
+          v-model="formData.mobile"
+          style="width: 50%"
+          placeholder="请输入手机号"
+        />
       </el-form-item>
       <el-form-item label="入职时间" prop="timeOfEntry">
-        <el-date-picker v-model="formData.timeOfEntry" style="width:50%" placeholder="请选择入职时间" />
+        <el-date-picker
+          v-model="formData.timeOfEntry"
+          style="width: 50%"
+          placeholder="请选择入职时间"
+        />
       </el-form-item>
       <el-form-item label="聘用形式" prop="formOfEmployment">
-        <el-select v-model="formData.formOfEmployment" style="width:50%" placeholder="请选择" />
+        <el-select
+          v-model="formData.formOfEmployment"
+          style="width: 50%"
+          placeholder="请选择"
+        />
       </el-form-item>
       <el-form-item label="工号" prop="workNumber">
-        <el-input v-model="formData.workNumber" style="width:50%" placeholder="请输入工号" />
+        <el-input
+          v-model="formData.workNumber"
+          style="width: 50%"
+          placeholder="请输入工号"
+        />
       </el-form-item>
       <el-form-item label="部门" prop="departmentName">
-        <el-input v-model="formData.departmentName" style="width:50%" placeholder="请选择部门" @focus="getDepartments" />
-        <el-tree v-if="treeData.length>0" :data="treeData" :props="{label:'name'}" :default-expand-all="true" />
+        <el-input
+          v-model="formData.departmentName"
+          style="width: 50%"
+          placeholder="请选择部门"
+          @focus="getDepartments"
+        />
+        <div v-if="treeData.length > 0" class="tree-wrapper">
+          <el-tree
+            :data="treeData"
+            :props="{ label: 'name' }"
+            :default-expand-all="true"
+            class="tree"
+          />
+        </div>
       </el-form-item>
       <el-form-item label="转正时间" prop="correctionTime">
-        <el-date-picker v-model="formData.correctionTime" style="width:50%" placeholder="请选择转正时间" />
+        <el-date-picker
+          v-model="formData.correctionTime"
+          style="width: 50%"
+          placeholder="请选择转正时间"
+        />
       </el-form-item>
     </el-form>
     <!-- footer插槽 -->
@@ -65,15 +101,31 @@ export default {
         correctionTime: ''
       },
       rules: {
-        username: [{ required: true, message: '用户姓名不能为空', trigger: 'blur' }, {
-          min: 1, max: 4, message: '用户姓名为1-4位'
-        }],
-        mobile: [{ required: true, message: '手机号不能为空', trigger: 'blur' }, {
-          pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: 'blur'
-        }],
-        formOfEmployment: [{ required: true, message: '聘用形式不能为空', trigger: 'blur' }],
-        workNumber: [{ required: true, message: '工号不能为空', trigger: 'blur' }],
-        departmentName: [{ required: true, message: '部门不能为空', trigger: 'change' }],
+        username: [
+          { required: true, message: '用户姓名不能为空', trigger: 'blur' },
+          {
+            min: 1,
+            max: 4,
+            message: '用户姓名为1-4位'
+          }
+        ],
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: '手机号格式不正确',
+            trigger: 'blur'
+          }
+        ],
+        formOfEmployment: [
+          { required: true, message: '聘用形式不能为空', trigger: 'blur' }
+        ],
+        workNumber: [
+          { required: true, message: '工号不能为空', trigger: 'blur' }
+        ],
+        departmentName: [
+          { required: true, message: '部门不能为空', trigger: 'change' }
+        ],
         timeOfEntry: [{ required: true, message: '入职时间', trigger: 'blur' }]
       }
       // defaultProps: {
@@ -89,11 +141,22 @@ export default {
       this.treeData = convertTreeData(depts, '')
       console.log(this.treeData)
     }
-
   }
 }
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+.tree-wrapper{
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  border: 1px solid #ccc;
+  overflow: hidden;
+  .tree{
+    width: 317px;
+    height: 300px;
+    overflow-y: scroll;
+    position: absolute;
+    z-index: 999;
+  }
+}
 </style>
