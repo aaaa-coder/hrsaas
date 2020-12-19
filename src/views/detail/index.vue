f50ae50a6b5f2ef3568b69a7ed3c1dea33ec73ec
tangchaofu <1027402361@qq.com>
2020-12-19 21:03:26 +0800

    '添加详情'

---
+++ src/views/detail/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-tabs v-model="employeesInfo">
+        <el-tab-pane label="登录账户管理" name="acount">用户管理</el-tab-pane>
+        <el-tab-pane label="个人详情" name="detail">配置管理</el-tab-pane>
+        <el-tab-pane label="岗位详情" name="job">角色管理</el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      employeesInfo: 'acount'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
