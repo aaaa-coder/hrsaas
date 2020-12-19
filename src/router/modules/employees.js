12f53e055234d17e5e1c939fe99345c606ed96a8
tangchaofu <1027402361@qq.com>
2020-12-19 20:58:53 +0800

    '根据id进行跳转'

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -2,10 +2,18 @@ import Layout from '@/layout'
 export default {
   path: '/employees',
   component: Layout,
-  children: [{
-    path: '',
-    component: () => import('@/views/employees/index'),
-    meta: { title: '员工', icon: 'people' }
-  }]
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/employees/index'),
+      meta: { title: '员工', icon: 'people' }
+    },
+    {
+      path: 'detail:id',
+      hidden: true,
+      component: () => import('@/views/detail'),
+      meta: { title: '员工详情' }
+    }
+  ]
 }
 
