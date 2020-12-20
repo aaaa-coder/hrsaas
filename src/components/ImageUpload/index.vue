670697f4399938699835f58cab34593e25609975
tangchaofu <1027402361@qq.com>
2020-12-20 20:58:54 +0800

    '修复图片变形问题'

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -6,26 +6,42 @@
       :file-list="fileList"
       :on-preview="onPreview"
       :limit="1"
+      class="uploader"
     >
       <i class="el-icon-plus" />
     </el-upload>
+    <el-dialog title="预览图片" :visible.sync="showDialog" />
   </div>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      fileList: []
+      fileList: [{
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608375246691&di=a35d6a6212159f60d18a8396d1ec8e20&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F03%2F36%2F15%2F5b8dd7ac4164e_610.jpg'
+      }], // 文件列表
+      showDialog: false
     }
   },
   methods: {
     onPreview(file) {
       console.log(file)
+      this.showDialog = true
     }
   }
 }
 </script>
-
-<style>
+<style lang="scss" scoped>
+.uploader {
+  ::v-deep img {
+    object-fit: cover;
+  }
+}
+.disable {
+  ::v-deep .el-upload--picture-card {
+    display: none;
+  }
+}
 </style>
