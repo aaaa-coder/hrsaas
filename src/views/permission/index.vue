3072113731e1d8f3522a195d2b9aec6d5e56aae0
tangchaofu <1027402361@qq.com>
2020-12-22 19:59:39 +0800

    '点击新增会弹出显示框'

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -14,14 +14,42 @@
         <el-table-column align="center" label="权限标识" prop="code" />
         <el-table-column align="center" label="权限描述" prop="description" />
         <el-table-column align="center" label="操作">
-          <template>
-            <el-button type="primary" size="mini">添加</el-button>
+          <template slot-scope="{row}">
+            <el-button type="primary" size="mini" @click="addPermission(2,row.id)">添加</el-button>
             <el-button type="primary" size="mini">编辑</el-button>
             <el-button type="primary" size="mini">删除</el-button>
           </template>
         </el-table-column>
 
       </el-table>
+      <!-- 放置一个弹层 用来编辑新增节点 -->
+      <el-dialog :visible="showDialog" title="新增">
+        <!-- 表单 -->
+        <el-form label-width="120px" :data="formData">
+          <el-form-item label="权限名称" prop="name">
+            <el-input v-model="formData.name" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限标识" prop="code">
+            <el-input v-model="formData.code" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限描述">
+            <el-input v-model="formData.description" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="开启">
+            <el-switch
+              v-model="formData.enVisible"
+              active-value="1"
+              inactive-value="0"
+            />
+          </el-form-item>
+        </el-form>
+        <el-row slot="footer" type="flex" justify="center">
+          <el-col :span="6">
+            <el-button size="small" type="primary">确定</el-button>
+            <el-button size="small">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -44,7 +72,8 @@ export default {
         description: '',
         pid: '',
         enVisible: ''
-      }
+      },
+      showDialog: false
     }
   },
   created() {
@@ -55,6 +84,9 @@ export default {
     async getPermissionList() {
       this.list = convertTreeData(await getPermissionList(), '0')
       console.log(this.list)
+    },
+    addPermission(type, id) {
+      this.showDialog = true
     }
   }
 
