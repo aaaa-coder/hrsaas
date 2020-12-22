1d60479fd204e6103a8f658ff28a01265723f735
tangchaofu <1027402361@qq.com>
2020-12-22 09:48:01 +0800

    '创建打印页面'

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -13,6 +13,12 @@ export default {
       hidden: true,
       component: () => import('@/views/employees/detail'),
       meta: { title: '员工详情' }
+    },
+    {
+      path: 'print/:id',
+      component: () => { import ('@/views/employees/print') },
+      hidden: true,
+      meta: { title: '打印页面' }
     }
   ]
 }
